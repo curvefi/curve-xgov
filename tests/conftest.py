d1526717a795b515a09accbdddc3d27c1aaae63e
Edward Amor <edward.amor3@gmail.com>
2022-10-08 05:03:01 -0400

    test: send_message

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,11 @@ def alice(accounts):
     yield accounts[0]
 
 
+@pytest.fixture(scope="session")
+def bob(accounts):
+    yield accounts[1]
+
+
 # optimism fixtures
 
 
--- tests/test_optimism_proxy.py
+++ tests/test_optimism_proxy.py
@@ -1,3 +1,4 @@
+import ape
 import eth_abi
 
 ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
@@ -23,3 +24,18 @@ def test_send_message_success(
     assert cross_domain_messenger.call_count() == 1
     target = next(filter(lambda frame: frame.op == "STATICCALL", tx.trace)).stack[-2]
     assert eth_abi.decode_single("address", target) == canonical_transaction_chain
+
+
+def test_send_message_supplying_gas_limit(alice, l1_optimism_proxy, canonical_transaction_chain):
+    tx = l1_optimism_proxy.send_message(alice, b"", 42, sender=alice)
+
+    staticcall_targets = {
+        eth_abi.decode_single("address", call.stack[-2])
+        for call in filter(lambda frame: frame.op == "STATICCALL", tx.trace)
+    }
+    assert canonical_transaction_chain.address not in staticcall_targets
+
+
+def test_send_message_failure_incorrect_caller(bob, l1_optimism_proxy):
+    with ape.reverts():
+        l1_optimism_proxy.send_message(bob, b"", sender=bob)
