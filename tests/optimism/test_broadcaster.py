777dd927b3459ecebf38fd7f9d3cc4a0e06f809c
Edward Amor <edward.amor3@gmail.com>
2022-10-09 16:17:31 -0400

    test: set_ovm methods

--- tests/optimism/test_broadcaster.py
+++ tests/optimism/test_broadcaster.py
@@ -11,6 +11,32 @@ def test_constructor(alice, bob, charlie, project, mock_chain, mock_messenger):
     assert broadcaster.ovm_messenger() == mock_messenger
 
 
+def test_set_ovm_chain(alice, broadcaster, ZERO_ADDRESS):
+    tx = broadcaster.set_ovm_chain(ZERO_ADDRESS, sender=alice)
+
+    assert broadcaster.ovm_chain() == ZERO_ADDRESS
+    assert len(tx.logs) == 1
+    assert tx.logs[0]["topics"][0] == keccak("SetOVMChain(address)".encode())
+
+
+def test_set_ovm_chain_reverts(bob, broadcaster, ZERO_ADDRESS):
+    with ape.reverts():
+        broadcaster.set_ovm_chain(ZERO_ADDRESS, sender=bob)
+
+
+def test_set_ovm_messenger(alice, broadcaster, ZERO_ADDRESS):
+    tx = broadcaster.set_ovm_messenger(ZERO_ADDRESS, sender=alice)
+
+    assert broadcaster.ovm_messenger() == ZERO_ADDRESS
+    assert len(tx.logs) == 1
+    assert tx.logs[0]["topics"][0] == keccak("SetOVMMessenger(address)".encode())
+
+
+def test_set_ovm_messenger_reverts(bob, broadcaster, ZERO_ADDRESS):
+    with ape.reverts():
+        broadcaster.set_ovm_messenger(ZERO_ADDRESS, sender=bob)
+
+
 def test_commit_admins(alice, bob, charlie, broadcaster):
     tx = broadcaster.commit_admins((alice, bob, charlie), sender=alice)
 
